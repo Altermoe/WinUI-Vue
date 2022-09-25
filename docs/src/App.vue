48e2e4f5265f9f151951aaab779466306404903c
夕云葛城 <kuroneko7520@outlook.com>
2022-09-25 16:31:27 +0800

    build: init

---
+++ docs/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const title = 'Coming soon'
+const content = 'Vue3 implementation of Windows 11 UI Design Principles.'
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ content }}</p>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+
+export default defineConfig((/** context */) => {
+  return {
+    root: __dirname,
+
+    envDir: resolve(__dirname, './env'),
+
+    resolve: {
+      alias: [
+        {
+          find: 'winui-vue',
+          replacement: resolve(__dirname, './src'),
+        },
+      ],
+    },
+
+    build: {
+      lib: {
+        entry: resolve(__dirname, './src/index.ts'),
+        name: 'WinUI-Vue',
+        fileName: 'winui-vue',
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      },
+    },
+
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment',
+    },
+
+    plugins: [Vue(), VueJsx()],
+  }
+})
