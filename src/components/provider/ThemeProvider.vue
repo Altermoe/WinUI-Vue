f6c839c2bd7ddab9b5767218d114ee3033aac80b
夕云葛城 <kuroneko7520@outlook.com>
2022-09-30 17:56:30 +0800

    style: add font

--- src/components/provider/ThemeProvider.vue
+++ src/components/provider/ThemeProvider.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useStyleTag } from '@vueuse/core'
 import { createStyle, getDefaultLightTheme } from '@/theme'
+import '@/styles/font.scss'
 
 const css = createStyle(getDefaultLightTheme())
 const { unload } = useStyleTag(css)
