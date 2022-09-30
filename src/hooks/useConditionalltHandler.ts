90600f92949083321340997e7499a8b40744d86a
夕云葛城 <kuroneko7520@outlook.com>
2022-09-30 16:49:15 +0800

    feat: add preventDefault

--- src/hooks/useConditionalltHandler.ts
+++ src/hooks/useConditionalltHandler.ts
@@ -2,14 +2,17 @@ import type { Ref } from 'vue'
 
 export interface EventHandlerHookOptions {
   disabled?: Ref<boolean> | boolean
+  preventDefault?: boolean
 }
 
 export const useConditionalltHandler = (emits: (...args: any[]) => any, options: EventHandlerHookOptions = {}) => {
-  const { disabled = false } = options
+  const { disabled = false, preventDefault = false } = options
 
   const handler = (e: string, ev: Event) => {
-    if (unref(disabled))
+    if (unref(disabled)) {
+      preventDefault && ev.preventDefault()
       return
+    }
     emits(e, ev)
   }
 
