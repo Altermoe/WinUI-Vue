e889620b1b7714b34eb50e621194f75fde88053a
夕云葛城 <kuroneko7520@outlook.com>
2022-10-08 00:01:53 +0800

    docs: add todo

--- src/components/layer/Layer.vue
+++ src/components/layer/Layer.vue
@@ -13,6 +13,7 @@ withDefaults(defineProps<{
 </template>
 
 <style lang="scss" scoped>
+// TODO: 这个组件与 Base 似乎有点定位重复
 .layer {
   background: rgba(255, 255, 255, 0.5);
   border: 1px solid rgba(0, 0, 0, 0.0578);
