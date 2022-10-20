545fd03a21ddc36ef8e88e4789c6ccc5d1526dde
夕云葛城 <kuroneko7520@outlook.com>
2022-10-20 16:59:47 +0800

    feat: navmenu 基本样式

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,20 +1,12 @@
 <script lang="ts" setup>
-import { Base, Dropdown, ThemeProvider } from '@/components'
-import type { DropdownItem } from '@/components'
-
-const value = ref('shanghai')
-
-const items: DropdownItem[] = [
-  { title: '上海', key: 'shanghai', value: 'shanghai' },
-  { title: '广州', key: 'guanghzou', value: 'guangzhou ' },
-]
+import { NavMenu, ThemeProvider } from '@/components'
 </script>
 
 <template>
   <ThemeProvider>
     <div style="display: flex; flex-direction: column; align-items: center; padding-top: 1em ; width: 100%; height: 100%">
       <div style="display: flex; flex-direction: column; gap: 1em">
-        <Dropdown v-model="value" :items="items" />
+        <NavMenu />
       </div>
     </div>
   </ThemeProvider>
---
+++ src/components/navMenu/NavMenu.vue
@@ -0,0 +1,106 @@
+<script lang="ts" setup>
+const activeIndex = ref(-1)
+</script>
+
+<template>
+  <div class="win-navmenu" v-bind="$attrs">
+    <div
+      v-for="i in 5" :key="i"
+      class="win-navmenu-item"
+      :class="{
+        active: activeIndex === i,
+        disabled: i === 2,
+      }"
+      @click="activeIndex = i"
+    >
+      <div class="win-navmenu-item-wrapper">
+        <div class="icon">
+          
+        </div>
+        <div class="content">
+          {{ i }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.win-navmenu {
+  --item-border-color: transparent;
+  --item-bg: transparent;
+  --item-color: rgba(0, 0, 0, 0.9);
+  --item-cursor: pointer;
+  --item-marker: transparent;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.win-navmenu-item {
+  padding: 2px 4px;
+  cursor: var(--item-cursor);
+  color: var(--item-color);
+
+  &.disabled {
+    --item-cursor: not-allowed;
+    --item-color: rgba(0, 0, 0, 0.36);
+    &.active {
+      --item-bg: rgba(0, 0, 0, 0.0373);
+      --item-marker: rgba(0, 0, 0, 0.2169);
+    }
+  }
+
+  &:not(.disabled) {
+    &:hover {
+      --item-bg: rgba(0, 0, 0, 0.0373);
+    }
+    &:active {
+      --item-bg: rgba(0, 0, 0, 0.0241);
+      --item-color: rgba(0, 0, 0, 0.61);
+    }
+    &.active {
+      --item-bg: rgba(0, 0, 0, 0.0373);
+      --item-marker: rgba(0, 95, 184, 1);
+    }
+  }
+}
+
+.win-navmenu-item-wrapper {
+  display: flex;
+  position: relative;
+  height: 36px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  padding: 9px 11px;
+  border-radius: 4px;
+  border: 1px solid var(--item-border-color);
+  background: var(--item-bg);
+  box-sizing: border-box;
+  font-size: 16px;
+
+  &::before {
+    content: '';
+    pointer-events: none;
+    position: absolute;
+    left: -1px;
+    top: 25%;
+    height: 16px;
+    width: 3px;
+    border-radius: 3px;
+    background: var(--item-marker);
+  }
+
+  .icon {
+    font-family: 'Segoe-UI', sans-serif;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .content {
+    padding-left: 1em;
+    height: 20px;
+    line-height: 20px;
+  }
+}
+</style>
