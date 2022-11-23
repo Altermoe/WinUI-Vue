44dff0eec8388900fb70e832585253cfdf953d78
夕云葛城 <kuroneko7520@outlook.com>
2022-11-23 20:53:33 +0800

    feat: rating score

--- src/components/rating/Rating.vue
+++ src/components/rating/Rating.vue
@@ -19,16 +19,71 @@ const internalBind = computed({
     emits('update:modelValue', v)
   },
 })
+
+const containerRef = ref<HTMLElement | null>(null)
+const isHover = useElementHover(containerRef)
+
+const hoverRating = ref(0)
+const switchStar = (ratio: number) => {
+  if (!isHover.value)
+    return ''
+  return ratio > 0.75
+    ? ''
+    : ratio > 0.5
+      ? ''
+      : ratio > 0.25
+        ? ''
+        : ratio > 0
+          ? ''
+          : ''
+}
+
+const hoverRate = (ev: MouseEvent, index: number) => {
+  hoverRating.value = index + ev.offsetX / (ev.target as HTMLElement).clientWidth
+}
 </script>
 
 <template>
-  <div class="win-rating">
-    Rating
+  <div ref="containerRef" class="win-rating">
+    <div
+      v-for="i in 5"
+      :key="i"
+      :data-mask="switchStar(hoverRating - i)"
+      class="win-rating__item win-icon w-full h-full"
+      @mousemove="(ev) => hoverRate(ev, i)"
+    >
+      
+    </div>
+    {{ hoverRating }}
   </div>
 </template>
 
 <style lang="scss" scoped>
 .win-rating {
+  --rating-item-size:  24px;
+
   display: flex;
+}
+
+.win-rating__item {
+  width: var(--rating-item-size);
+  height: var(--rating-item-size);
+  display: grid;
+  place-items: center;
+  user-select: none;
+  font-size: 16px;
+  cursor: pointer;
+  position: relative;
+
+  &::before {
+    content: attr(data-mask);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    display: grid;
+    place-items: center;
+  }
 }
 </style>
