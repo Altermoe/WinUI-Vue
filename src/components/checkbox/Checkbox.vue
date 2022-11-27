a364261157f13d4a6f37fa78f3183d9a2ab703ac
夕云葛城 <kuroneko7520@outlook.com>
2022-11-27 20:32:17 +0800

    feat: checkbox base props

--- src/components/checkbox/Checkbox.vue
+++ src/components/checkbox/Checkbox.vue
@@ -1,7 +1,32 @@
 <script lang="ts" setup>
-defineProps<{
-  modelValue?: boolean
+type CheckboxValue = string | number
+
+interface CheckboxOption {
+  label?: string
+  value: CheckboxValue
+}
+
+const props = defineProps<{
+  modelValue?: CheckboxValue[]
+  options?: CheckboxOption[]
 }>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', v: CheckboxValue[]): void
+}>()
+
+const cache = ref<CheckboxValue[]>([])
+
+const internalBind = computed({
+  get: () => props.modelValue === undefined ? cache.value : props.modelValue,
+  set: (v) => {
+    if (props.modelValue === undefined) {
+      cache.value = v
+      return
+    }
+    emits('update:modelValue', v)
+  },
+})
 </script>
 
 <template>
