2d84f6c6c74c695bba4af71c022633303da5bf2d
夕云葛城 <kuroneko7520@outlook.com>
2022-11-29 23:47:11 +0800

    feat: chexkbox marker style

--- src/components/checkbox/Checkbox.vue
+++ src/components/checkbox/Checkbox.vue
@@ -30,5 +30,38 @@ const internalBind = computed({
 </script>
 
 <template>
-  <div>checkbox</div>
+  <div class="win-checkbox checked">
+    <div class="win-checkbox__marker" />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.win-checkbox {
+  --cursor: pointer;
+  --marker-bg-color: rgba(0, 0, 0, 0.0241);
+  --marker-border-color: rgba(0, 0, 0, 0.4458);
+
+  &.disabled {
+    --cursor: not-allowed;
+    --marker-bg-color: transparent;
+  }
+
+  &:hover {
+    --marker-bg-color: rgba(0, 0, 0, 0.0578);
+  }
+
+  &:active {
+    --marker-bg-color: rgba(0, 0, 0, 0.0924);
+    --marker-border-color: rgba(0, 0, 0, 0.2169);
+  }
+}
+
+.win-checkbox__marker {
+  width: 20px;
+  height: 20px;
+  border: 1px solid var(--marker-border-color);
+  border-radius: 4px;
+  background-color: var(--marker-bg-color);
+  cursor: var(--cursor);
+}
+</style>
