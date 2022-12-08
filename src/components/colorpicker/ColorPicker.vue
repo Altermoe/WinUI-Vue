84ec78e81e0a2bbae2e482f9f5d0a88594aea1dd
夕云葛城 <kuroneko7520@outlook.com>
2022-12-08 22:34:46 +0800

    feat: colormap for colorpicker

--- src/components/colorpicker/ColorPicker.vue
+++ src/components/colorpicker/ColorPicker.vue
@@ -1,15 +1,55 @@
 <script lang="ts" setup>
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   modelValue?: string
-}>()
+  type?: 'rgba' | 'hsl'
+}>(), {
+  type: 'rgba',
+})
 
 const emits = defineEmits<{
   (e: 'update:modelValue', v?: string): void
 }>()
+
+const colormapRef = ref<HTMLCanvasElement | null>(null)
+const { width, height } = useElementSize(colormapRef)
+const colormapCtx = computed(() => colormapRef.value?.getContext('2d'))
+
+const drawBackground = ([w, h]: [number, number]) => {
+  const ctx = colormapCtx.value
+  if (!ctx)
+    throw new Error('无法获取绘制上下文')
+  ctx.save()
+  for (let i = 0; i < 256; i++) {
+    const gradient = ctx.createLinearGradient(0, 0, 1, 255)
+    gradient.addColorStop()
+    ctx.fillStyle = '#000'
+    ctx.fillRect(0, 0, w, h)
+  }
+  ctx.restore()
+}
+
+useResizeObserver(colormapRef, (entry) => {
+  const { width, height } = entry[0].contentRect
+  drawBackground([width, height])
+})
 </script>
 
 <template>
-  <div class="win-colorpicker">
-    ColorPicker
+  <div class="win-colorpicker" :class="{ round: type === 'hsl' }">
+    <canvas
+      ref="colormapRef"
+      class="win-colorpicker__colormap"
+      :width="width"
+      :height="height"
+    />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.win-colorpicker__colormap {
+  display: inline-block;
+  width: 256px;
+  height: 256px;
+  overflow: hidden;
+}
+</style>
