5b30abb8e7e325cac4ea7d21ef61f8d1bed893ce
夕云葛城 <kuroneko7520@outlook.com>
2022-12-26 22:23:30 +0800

    feat: 抽象列表组件

---
+++ src/components/list/List.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+/** 该组件用于抽象 winui 组件库中所有的列表类组件的共用样式和交互效果 */
+</script>
+
+<template>
+  <div class="win-list">
+    抽象列表组件
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.win-list {
+  display: flex;
+}
+</style>
